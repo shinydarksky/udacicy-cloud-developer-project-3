076cab9f7283504811d0c6fad14040bf9e2aa277
Lelouch <shinydark.sky@gmail.com>
2023-03-06 19:22:53 +0700

    update frontend api

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a942c229d0c6a4322bc14be6989a00bd-920020263.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
